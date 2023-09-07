e897152c84f2d29f69200f90d2348c9b1771e86c
ascotlan <antonio.k.scotland@gmail.com>
2023-09-07 00:54:11 -0400

    Implement data persistence with API requests

--- src/App.js
+++ src/App.js
@@ -1,40 +1,53 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import BookCreate from "./components/BookCreate";
-import BookList from "./components/BookList"
+import BookList from "./components/BookList";
+import axios from "axios";
 
 function App() {
   const [books, setBooks] = useState([]);
 
-  const deleteBookById = (id) => {
-    const updatedBooks = books.filter((book) => book.id !== id) 
+  const fetchBooks = async () => {
+    const response = await axios.get("http://localhost:3001/books");
+    setBooks(response.data);
+  };
+
+  useEffect(() => {
+    fetchBooks();
+  }, []);
+
+  const deleteBookById = async (id) => {
+    await axios.delete(`http://localhost:3001/books/${id}`);
+    const updatedBooks = books.filter((book) => book.id !== id);
     setBooks(updatedBooks);
   };
 
-  const editBookById = (id, title) => {
-    const updatedBooks = books.map((book) =>{
-      if(book.id === id){
-        return {...book, title}
+  const editBookById = async (id, title) => {
+    const response = await axios.put(`http://localhost:3001/books/${id}`, {
+      title,
+    });
+
+    const updatedBooks = books.map((book) => {
+      if (book.id === id) {
+        return { ...book, ...response.data };
       }
 
       return book;
-    }) 
+    });
     setBooks(updatedBooks);
-  }
+  };
 
-  const createBook = (title) => {
-    setBooks([
-      ...books,
-      {
-        id: Math.round(Math.random() * 9999),
-        title,
-      },
-    ]);
+  const createBook = async (title) => {
+    const response = await axios.post("http://localhost:3001/books", {
+      title,
+    });
+
+    setBooks([...books, response.data]);
   };
 
   return (
     <section className="app">
-    <h1>Reading List</h1>
-      <BookList books={books} onDelete={deleteBookById} onEdit={editBookById}/>
+      <h1>Reading List</h1>
+      <BookList books={books} onDelete={deleteBookById} onEdit={editBookById} />
       <BookCreate onCreate={createBook} />
     </section>
   );
